83608eb179024cd63fefdb09e73c13e040a4a093
Gustavo <gustavojose0819@gmail.com>
2025-01-17 22:59:54 -0400

    16vo Commit

---
+++ controllers/getCountryByIp.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const getCountryByIp = async (ip) => {
+  const apiKey = process.env.IPSTACK_API_KEY; 
+  const url = `http://api.ipstack.com/${ip}?access_key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data.country_name;
+  } catch (error) {
+    console.error("Error al obtener el país:", error.message);
+    return undefined;
+  }
+};
+
+module.exports = { getCountryByIp };

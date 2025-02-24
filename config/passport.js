a6156120c62eeb691ea89dcef56ab4bfd4e70b6f
Gustavo <gustavojose0819@gmail.com>
2025-02-24 01:06:53 -0400

    50mo Commit

--- config/passport.js
+++ config/passport.js
@@ -32,5 +32,41 @@ passport.deserializeUser((id, done) => {
         done(err, user);
     });
 });
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const UserModel = require('../models/UserModel');
+
+// Autenticación local
+passport.use(new LocalStrategy((username, password, done) => {
+    UserModel.autenticarUsuario(username, password, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: 'Usuario o contraseña incorrectos' });
+        return done(null, user);
+    });
+}));
+
+// Autenticación con Google
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://p2-31-182-936-acti-2.onrender.com/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+    UserModel.findOrCreateGoogleUser(profile, (err, user) => {
+        return done(err, user);
+    });
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    UserModel.getUserById(id, (err, user) => {
+        done(err, user);
+    });
+});
+
+module.exports = passport;
 
 module.exports = passport;
